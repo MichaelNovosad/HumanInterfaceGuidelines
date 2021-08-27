12167c6779f9630031c720e48f68808fd36af3e1
Michael Novosad <michael.novosad@outlook.com>
2021-08-27 10:38:48 +0300

    Initial Commit

---
+++ HumanInterfaceGuidelines/HumanInterfaceGuidelinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanInterfaceGuidelinesApp.swift
+//  HumanInterfaceGuidelines
+//
+//  Created by Michael Novosad on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct HumanInterfaceGuidelinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
